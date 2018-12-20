6dcaf486a28d5cfd5daad2e368104b4cda9bc23f
zhupei <zhupei.pur@outlook.com>
2018-12-20 23:47:22 +0800

    Curl

--- publlic/Curl.cpp
+++ publlic/Curl.cpp
@@ -1,5 +1,225 @@
 #include "Curl.h"
 
-curl::Request::Request(Curl *, const char * url)
+#include <muduo/base/Logging.h>
+#include <muduo/net/Channel.h>
+#include <muduo/net/EventLoop.h>
+
+curl::Request::Request(Curl * owner, const char * url)
+	:owner_(owner),
+	curl_(CHECK_NOTNULL(curl_easy_init()))
 {
+	setopt(CURLOPT_URL, url);
+	setopt(CURLOPT_WRITEFUNCTION, &Request::writeData);
+	setopt(CURLOPT_WRITEDATA, this);
+	setopt(CURLOPT_HEADERFUNCTION, &Request::headerData);
+	setopt(CURLOPT_HEADERDATA, this);
+	setopt(CURLOPT_PRIVATE, this);
+	setopt(CURLOPT_USERAGENT, "curl");
+
+	LOG_DEBUG << curl_ << " " << url;
+	curl_multi_add_handle(owner_->getCurlm(), curl_);
+}
+
+curl::Request::~Request()
+{
+	assert(!channel_ || channel_->isNoneEvent());
+	curl_multi_remove_handle(owner_->getCurlm(), curl_);
+	curl_easy_cleanup(curl_);
+}
+
+muduo::net::Channel * curl::Request::setChannel(int fd)
+{
+	assert(channel_.get() == NULL);
+	channel_.reset(new muduo::net::Channel(owner_->getLoop(), fd));
+	channel_->tie(shared_from_this());
+	return get_pointer(channel_);
+}
+
+void curl::Request::removeChannel()
+{
+	channel_->disableAll();
+	channel_->remove();
+	//owner_->getLoop()->queueInLoop(std::bind(dummy, channel_));
+	channel_.reset();
+}
+
+void curl::Request::done(int code)
+{
+	if (doneCb_)
+	{
+		doneCb_(this, code);
+	}
+}
+
+void curl::Request::dataCallback(const char * buffer, int len)
+{
+	if (dataCb_)
+	{
+		dataCb_(buffer, len);
+	}
+}
+
+void curl::Request::headerCallback(const char * buffer, int len)
+{
+	if (headerCb_)
+	{
+		headerCb_(buffer, len);
+	}
+}
+
+size_t curl::Request::writeData(char * buffer, size_t size, size_t nmemb, void * userp)
+{
+	assert(size == 1);
+	Request* req = static_cast<Request*>(userp);
+	req->dataCallback(buffer, static_cast<int>(nmemb));
+	return nmemb;
+}
+
+size_t curl::Request::headerData(char * buffer, size_t size, size_t nmemb, void * userp)
+{
+	assert(size == 1);
+	Request* req = static_cast<Request*>(userp);
+	req->headerCallback(buffer, static_cast<int>(nmemb));
+	return nmemb;
+}
+
+void curl::Request::doneCallback()
+{
+}
+
+// ==================================================================
+
+curl::Curl::Curl(muduo::net::EventLoop * loop)
+	:loop_(loop),
+	curlm_(CHECK_NOTNULL(curl_multi_init())),
+	runningHandles_(0),
+	prevRunningHandles_(0)
+{
+	curl_multi_setopt(curlm_, CURLMOPT_SOCKETFUNCTION, &Curl::socketCallback);
+	curl_multi_setopt(curlm_, CURLMOPT_SOCKETDATA, this);
+	curl_multi_setopt(curlm_, CURLMOPT_TIMERFUNCTION, &Curl::timerCallback);
+	curl_multi_setopt(curlm_, CURLMOPT_TIMERDATA, this);
+}
+
+
+curl::Curl::~Curl()
+{
+	curl_multi_cleanup(curlm_);
+}
+
+curl::RequestPtr curl::Curl::getRequest(muduo::StringArg url)
+{
+	return RequestPtr();
+}
+
+void curl::Curl::initialize(Option opt)
+{
+	curl_global_init(opt == kCURLnossl ? CURL_GLOBAL_NOTHING : CURL_GLOBAL_SSL);
+}
+
+void curl::Curl::onTimer()
+{
+	CURLMcode rc = CURLM_OK;
+	do {
+		LOG_TRACE;
+		rc = curl_multi_socket_action(curlm_, CURL_SOCKET_TIMEOUT, 0, &runningHandles_);
+		LOG_TRACE << rc << " " << runningHandles_;
+	} while (rc == CURLM_CALL_MULTI_PERFORM);
+	checkFinish();
+}
+
+void curl::Curl::onRead(int fd)
+{
+	CURLMcode rc = CURLM_OK;
+	do {
+		LOG_TRACE << fd;
+		rc = curl_multi_socket_action(curlm_, fd, CURL_POLL_IN, &runningHandles_);
+		LOG_TRACE << fd << " " << rc << " " << runningHandles_;
+	} while (rc == CURLM_CALL_MULTI_PERFORM);
+	checkFinish();
+}
+
+void curl::Curl::onWrite(int fd)
+{
+	CURLMcode rc = CURLM_OK;
+	do {
+		LOG_TRACE << fd;
+		rc = curl_multi_socket_action(curlm_, fd, CURL_POLL_OUT, &runningHandles_);
+		LOG_TRACE << fd << " " << rc << " " << runningHandles_;
+	} while (rc == CURLM_CALL_MULTI_PERFORM);
+	checkFinish();
+}
+
+void curl::Curl::checkFinish()
+{
+	if (prevRunningHandles_ > runningHandles_ || runningHandles_ == 0)
+	{
+		CURLMsg* msg = NULL;
+		int left = 0;
+		while ((msg = curl_multi_info_read(curlm_, &left)) != NULL)
+		{
+			if (msg->msg == CURLMSG_DONE)
+			{
+				CURL* c = msg->easy_handle;
+				CURLcode res = msg->data.result;
+				Request* req = NULL;
+				curl_easy_getinfo(c, CURLINFO_PRIVATE, &req);
+				assert(req->getCurl() == c);
+				LOG_TRACE << req << " done";
+				req->done(res);
+			}
+		}
+	}
+	prevRunningHandles_ = runningHandles_;
+}
+
+int curl::Curl::socketCallback(CURL* c, int fd, int what, void* userp, void* socketp)
+{
+	Curl* curl = static_cast<Curl*>(userp);
+	const char *whatstr[] = { "none", "IN", "OUT", "INOUT", "REMOVE" };
+	LOG_DEBUG << "Curl::socketCallback [" << curl << "] - fd = " << fd
+		<< " what = " << whatstr[what];
+	Request* req = NULL;
+	curl_easy_getinfo(c, CURLINFO_PRIVATE, &req);
+	assert(req->getCurl() == c);
+	if (what == CURL_POLL_REMOVE)
+	{
+		muduo::net::Channel* ch = static_cast<muduo::net::Channel*>(socketp);
+		assert(req->getChannel() == ch);
+		req->removeChannel();
+		ch = NULL;
+		curl_multi_assign(curl->curlm_, fd, ch);
+	}
+	else
+	{
+		muduo::net::Channel* ch = static_cast<muduo::net::Channel*>(socketp);
+		if (!ch)
+		{
+			ch = req->setChannel(fd);
+			ch->setReadCallback(std::bind(&Curl::onRead, curl, fd));
+			ch->setWriteCallback(std::bind(&Curl::onWrite, curl, fd));
+			ch->enableReading();
+			curl_multi_assign(curl->curlm_, fd, ch);
+			LOG_TRACE << "new channel for fd=" << fd;
+		}
+		assert(req->getChannel() == ch);
+		// update
+		if (what & CURL_POLL_OUT)
+		{
+			ch->enableWriting();
+		}
+		else
+		{
+			ch->disableWriting();
+		}
+	}
+	return 0;
+}
+
+int curl::Curl::timerCallback(CURLM* curlm, long ms, void* userp)
+{
+	Curl* curl = static_cast<Curl*>(userp);
+	LOG_DEBUG << curl << " " << ms << " ms";
+	curl->loop_->runAfter(static_cast<int>(ms) / 1000.0, std::bind(&Curl::onTimer, curl));
+	return 0;
 }
--- publlic/Curl.h
+++ publlic/Curl.h
@@ -1,3 +1,102 @@
 #pragma once
-
+#include <memory>
+#include <functional>
 #include <curl/curl.h>
+#include <muduo/net/Callbacks.h>
+#include <muduo/base/StringPiece.h>
+#include <muduo/base/noncopyable.h>
+namespace muduo::net
+{
+class Channel;
+class EventLoop;
+}
+namespace curl
+{
+class Curl;
+class Request :public std::enable_shared_from_this<Request>,
+	muduo::noncopyable
+{
+public:
+	typedef std::function<void(const char*, int)> DataCallback;
+	typedef std::function<void(Request*, int)> DoneCallback;
+	Request(Curl*, const char* url);
+	~Request();
+
+	void setDataCallback(const DataCallback& cb)
+	{
+		dataCb_ = cb;
+	}
+
+	void setDoneCallback(const DoneCallback& cb)
+	{
+		doneCb_ = cb;
+	}
+
+	void setHeaderCallback(const DataCallback& cb)
+	{
+		headerCb_ = cb;
+	}
+
+
+	template<typename OPT, typename P>
+	int setopt(OPT opt, P* p)
+	{
+		return curl_easy_setopt(curl_, opt, p);
+	}
+
+	// internal
+	muduo::net::Channel* setChannel(int fd);
+	void removeChannel();
+	void done(int code);
+	CURL* getCurl() { return curl_; }
+	muduo::net::Channel* getChannel() { return muduo::get_pointer(channel_); }
+private:
+	void dataCallback(const char* buffer, int len);
+	void headerCallback(const char* buffer, int len);
+	static size_t writeData(char *buffer, size_t size, size_t nmemb, void *userp);
+	static size_t headerData(char *buffer, size_t size, size_t nmemb, void *userp);
+	void doneCallback();
+
+	Curl* owner_;
+	CURL* curl_;
+	std::shared_ptr<muduo::net::Channel> channel_;
+	DataCallback dataCb_;
+	DataCallback headerCb_;
+	DoneCallback doneCb_;
+};
+
+typedef std::shared_ptr<Request> RequestPtr;
+class Curl : muduo::noncopyable
+{
+public:
+
+	enum Option
+	{
+		kCURLnossl = 0,
+		kCURLssl = 1,
+	};
+	explicit Curl(muduo::net::EventLoop* loop);
+	~Curl();
+
+	RequestPtr getRequest(muduo::StringArg url);
+
+	static void initialize(Option opt = kCURLnossl);
+
+	// internal
+	CURLM* getCurlm() { return curlm_; }
+	muduo::net::EventLoop* getLoop() { return loop_; }
+private:
+	void onTimer();
+	void onRead(int fd);
+	void onWrite(int fd);
+	void checkFinish();
+
+	static int socketCallback(CURL*, int, int, void*, void*);
+	static int timerCallback(CURLM*, long, void*);
+
+	muduo::net::EventLoop* loop_;
+	CURLM* curlm_;
+	int runningHandles_;
+	int prevRunningHandles_;
+};
+}
